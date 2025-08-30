d7e3ebf695d1de83256458ad9fb6c11dc4348fc0
John R Patek Sr <johnpatek2@gmail.com>
2025-08-30 12:27:39 -0500

    added import

--- protocol.py
+++ protocol.py
@@ -5,6 +5,7 @@ from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
+from luma.core.sprite_system import framerate_regulator
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 import socket
 
